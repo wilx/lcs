3a6222b3aebbe525615839ea510fc13a8d2f90ed
v.haisman <v.haisman@sh.cvut.cz>
2013-11-23 21:14:40 +0000

    white space cleanup

--- podpos2.cxx
+++ podpos2.cxx
@@ -25,12 +25,12 @@ lcs_b (_OutputIter ll, _InputIter1 ai, _InputIter1 end1,
       // step 3
       std::copy (k1.begin (), k1.end (), k0.begin ());
       // step 4
-      for (indexing_iterator<_InputIter2, _IndexType, _IndexPolicy> 
-	     bj (start2); bj.base () != end2; ++bj)
-	if (eq (*ai, *bj))
-	  k1[bj.index ()] = k0[bj.index () - 1] + 1;
-	else
-	  k1[bj.index ()] = std::max (k1[bj.index () - 1], k0[bj.index ()]);
+      for (indexing_iterator<_InputIter2, _IndexType, _IndexPolicy>
+             bj (start2); bj.base () != end2; ++bj)
+        if (eq (*ai, *bj))
+          k1[bj.index ()] = k0[bj.index () - 1] + 1;
+        else
+          k1[bj.index ()] = std::max (k1[bj.index () - 1], k0[bj.index ()]);
     }
   // step 5
   std::copy (k1.begin (), k1.end (), ll);
@@ -52,7 +52,7 @@ lcs_c (_OutputIter cc, _InputIter1 start1, _InputIter1 end1,
   else if (m == 1)
     {
       _InputIter2 const bj (std::find_if (start2, end2,
-					  std::bind1st (eq, *start1)));
+                                          std::bind1st (eq, *start1)));
       if (bj != end2)
         {
           *cc = *bj;
@@ -68,19 +68,19 @@ lcs_c (_OutputIter cc, _InputIter1 start1, _InputIter1 end1,
       size_t const i = m / 2;
       std::vector<size_t> l1 (n + 1);
       lcs_b (l1.begin (), start1,
-	     // i-1: count->offset; +1: end is one-past-the-end.
-	     start1 + i - 1 + 1, make_indexing_iterator (start2, 1), end2, eq);
-      
+             // i-1: count->offset; +1: end is one-past-the-end.
+             start1 + i - 1 + 1, make_indexing_iterator (start2, 1), end2, eq);
+
       std::vector<size_t> l2 (n + 1);
       lcs_b (l2.rbegin (), std::reverse_iterator<_InputIter1> (end1),
              std::reverse_iterator<_InputIter1> (start1 + i - 1 + 1),
              make_indexing_iterator (std::reverse_iterator<_InputIter2> (end2),
-				     n - 1),
+                                     n - 1),
              std::reverse_iterator<_InputIter2> (start2), eq);
       // step 4
-      size_t const mm = std::inner_product 
-	(l1.begin (), l1.end (), l2.rbegin (), static_cast<size_t>(0),
-	 std::ptr_fun<size_t const &, size_t const &, size_t const &>
+      size_t const mm = std::inner_product
+        (l1.begin (), l1.end (), l2.rbegin (), static_cast<size_t>(0),
+         std::ptr_fun<size_t const &, size_t const &, size_t const &>
          (std::max), std::plus<size_t> ());
 
       size_t k = static_cast<size_t>(-1);
@@ -92,23 +92,23 @@ lcs_c (_OutputIter cc, _InputIter1 start1, _InputIter1 end1,
           }
 
       // step 5
-      std::vector<typename std::iterator_traits<_InputIter1>::value_type> 
-	c1 (std::min (i, n));
-      size_t const c1len = lcs_c (c1.begin (), start1, start1 + i - 1 + 1, 
-				  start2, start2 + k - 1 + 1, eq);
+      std::vector<typename std::iterator_traits<_InputIter1>::value_type>
+        c1 (std::min (i, n));
+      size_t const c1len = lcs_c (c1.begin (), start1, start1 + i - 1 + 1,
+                                  start2, start2 + k - 1 + 1, eq);
 
-      std::vector<typename std::iterator_traits<_InputIter1>::value_type> 
-	c2 (std::min (m - i, n - k));
-      size_t const c2len = lcs_c (c2.begin (), start1 + i - 1 + 1, end1, 
-				  start2 + k - 1 + 1, end2, eq);
+      std::vector<typename std::iterator_traits<_InputIter1>::value_type>
+        c2 (std::min (m - i, n - k));
+      size_t const c2len = lcs_c (c2.begin (), start1 + i - 1 + 1, end1,
+                                  start2 + k - 1 + 1, end2, eq);
 
       typename std::vector<typename std::iterator_traits
-	<_InputIter1>::value_type>::const_iterator it;
+        <_InputIter1>::value_type>::const_iterator it;
       size_t u;
       for (it = c1.begin (), u = 0; u < c1len; ++u, ++it, ++cc)
-	*cc = *it;
+        *cc = *it;
       for (it = c2.begin (), u = 0; u < c2len; ++u, ++it, ++cc)
-	*cc = *it;
+        *cc = *it;
       return c1len + c2len;
     }
 }
@@ -121,13 +121,13 @@ int main ()
     std::string s2 ("abzc");
     //std::string s1 ("axbc");
     //std::string s2 ("abzc");
-    
+
     std::cout << "A: " << s1 << std::endl
-	      << "B: " << s2 << std::endl;
-    lcs_c (std::ostream_iterator<std::string::value_type> (std::cout, " "), 
-	   s1.begin (), s1.end (),
-	   s2.begin (), s2.end (), 
-	   std::equal_to<std::string::value_type> ());
+              << "B: " << s2 << std::endl;
+    lcs_c (std::ostream_iterator<std::string::value_type> (std::cout, " "),
+           s1.begin (), s1.end (),
+           s2.begin (), s2.end (),
+           std::equal_to<std::string::value_type> ());
     std::cout << std::endl;
   }
 
@@ -144,10 +144,10 @@ int main ()
     v2.push_back ("dummy\n");
     v2.push_back ("cccc\n");
 
-    
-    lcs_c (std::ostream_iterator<std::string> (std::cout, ""), 
-	   v1.begin (), v1.end (),
-	   v2.begin (), v2.end (), 
-	   std::equal_to<std::string> ());
+
+    lcs_c (std::ostream_iterator<std::string> (std::cout, ""),
+           v1.begin (), v1.end (),
+           v2.begin (), v2.end (),
+           std::equal_to<std::string> ());
   }
 }
--- podpos3.cxx
+++ podpos3.cxx
@@ -8,28 +8,28 @@
 #include "index_policy.hxx"
 #include "indexing_iterator.hxx"
 
-template <typename _OutputIter, typename _InputIter1, typename _InputIter2, 
+template <typename _OutputIter, typename _InputIter1, typename _InputIter2,
           typename _Eq, typename _IndexType, typename _IndexPolicy>
 void
 lcs_b (_OutputIter ll, _InputIter1 ai, _InputIter1 const & end1,
-       indexing_iterator<_InputIter2, _IndexType, _IndexPolicy> 
-       const & start2, 
-       indexing_iterator<_InputIter2, _IndexType, _IndexPolicy> const & end2, 
+       indexing_iterator<_InputIter2, _IndexType, _IndexPolicy>
+       const & start2,
+       indexing_iterator<_InputIter2, _IndexType, _IndexPolicy> const & end2,
        _Eq const & eq)
 {
   // step 1
   size_t const m = std::distance (ai, end1);
   size_t const n = std::distance (start2, end2);
   std::vector<size_t> k1 (n + 1, 0), k0 (n + 1);
-  
+
   // step 2
   for (; ai != end1; ++ai)
     {
       // step 3
       std::copy (k1.begin (), k1.end (), k0.begin ());
       // step 4
-      for (indexing_iterator<_InputIter2, _IndexType, _IndexPolicy> 
-	     bj (start2); bj != end2; ++bj)
+      for (indexing_iterator<_InputIter2, _IndexType, _IndexPolicy>
+             bj (start2); bj != end2; ++bj)
         {
           if (eq (*ai, *bj))
             k1[bj.index ()] = k0[bj.index () - 1] + 1;
@@ -42,7 +42,7 @@ lcs_b (_OutputIter ll, _InputIter1 ai, _InputIter1 const & end1,
 }
 
 
-template <typename _OutputIter, typename _InputIter1, typename _InputIter2, 
+template <typename _OutputIter, typename _InputIter1, typename _InputIter2,
           typename _Eq>
 size_t
 lcs_c (_OutputIter cc, _InputIter1 const & start1, _InputIter1 const & end1,
@@ -50,14 +50,14 @@ lcs_c (_OutputIter cc, _InputIter1 const & start1, _InputIter1 const & end1,
 {
   size_t const m = std::distance (start1, end1);
   size_t const n = std::distance (start2, end2);
-  
+
   // step 1
   if (n == 0)
     return 0;
   else if (m == 1)
     {
-      _InputIter2 const bj (std::find_if (start2, end2, 
-					  std::bind1st (eq, *start1)));
+      _InputIter2 const bj (std::find_if (start2, end2,
+                                          std::bind1st (eq, *start1)));
       if (bj != end2)
         {
           *cc = *bj;
@@ -65,7 +65,7 @@ lcs_c (_OutputIter cc, _InputIter1 const & start1, _InputIter1 const & end1,
         }
       else
         return 0;
-    }   
+    }
   // step 2
   else
     {
@@ -73,22 +73,22 @@ lcs_c (_OutputIter cc, _InputIter1 const & start1, _InputIter1 const & end1,
       size_t const i = m / 2;
       std::vector<size_t> l1 (n + 1);
       // i-1: count->offset; +1: end is one-past-the-end.
-      lcs_b (l1.begin (), start1, start1 + i - 1 + 1, 
-	     indexing_iterator<_InputIter2> (start2, 1), 
-	     indexing_iterator<_InputIter2> (end2), eq);
+      lcs_b (l1.begin (), start1, start1 + i - 1 + 1,
+             indexing_iterator<_InputIter2> (start2, 1),
+             indexing_iterator<_InputIter2> (end2), eq);
       std::vector<size_t> l2 (n + 1);
-      lcs_b (l2.begin (), std::reverse_iterator<_InputIter1> (end1), 
+      lcs_b (l2.begin (), std::reverse_iterator<_InputIter1> (end1),
              std::reverse_iterator<_InputIter1> (start1 + i - 1 + 1),
-             indexing_iterator<std::reverse_iterator<_InputIter2>, size_t, 
-	     reverse_index_policy<default_index_policy<size_t> > > 
-	     (std::reverse_iterator<_InputIter2> (end2), n), 
              indexing_iterator<std::reverse_iterator<_InputIter2>, size_t,
-	     reverse_index_policy<default_index_policy<size_t> > >
-	     (std::reverse_iterator<_InputIter2> (start2)), eq);
+             reverse_index_policy<default_index_policy<size_t> > >
+             (std::reverse_iterator<_InputIter2> (end2), n),
+             indexing_iterator<std::reverse_iterator<_InputIter2>, size_t,
+             reverse_index_policy<default_index_policy<size_t> > >
+             (std::reverse_iterator<_InputIter2> (start2)), eq);
       // step 4
-      size_t const mm = std::inner_product 
+      size_t const mm = std::inner_product
         (l1.begin (), l1.end (), l2.rbegin (), static_cast<size_t>(0),
-         std::ptr_fun<size_t const &, size_t const &, size_t const &> 
+         std::ptr_fun<size_t const &, size_t const &, size_t const &>
          (std::max), std::plus<size_t> ());
       size_t k;
       for (k = 0; k <= n; ++k)
@@ -96,10 +96,10 @@ lcs_c (_OutputIter cc, _InputIter1 const & start1, _InputIter1 const & end1,
           break;
       // step 5
       _OutputIter & tmp = cc;
-      size_t const c1len = lcs_c (tmp, start1, start1 + i - 1 + 1, start2, 
+      size_t const c1len = lcs_c (tmp, start1, start1 + i - 1 + 1, start2,
                                   start2 + k - 1 + 1, eq);
       return c1len + lcs_c (tmp , start1 + i - 1 + 1, end1, start2 + k - 1 + 1,
-			    end2, eq);
+                            end2, eq);
     }
 }
 
@@ -113,4 +113,3 @@ int main ()
   lcs_c (std::ostream_iterator<char> (std::cout, " "), s1.begin (), s1.end (),
          s2.begin (), s2.end (), std::equal_to<char>());
 }
-
